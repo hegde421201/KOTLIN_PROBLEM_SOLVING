2f9d48fb446a7567be807592366ba997bbc81dbd
hegde421201 <55439785+hegde421201@users.noreply.github.com>
2022-05-03 10:19:16 +0530

    latest
    latest

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.test {
@@ -25,5 +26,13 @@ tasks.withType<KotlinCompile>() {
 }
 
 application {
-   // mainClass.set("MainKt")
+    mainClass.set("MainKt")
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "17"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "17"
 }
